8d76f0f4c59c78f7608d285244b0abf94776e6e0
pandrio133 <pandrio133@gmail.com>
2025-03-04 15:05:50 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//estructura
+using System.Data.SqlClient;
+
+//logica
+namespace pruebaConexion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
