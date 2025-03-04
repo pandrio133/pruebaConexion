192d70ac2a10812a1aa247e77c4a31817bfd820d
pandrio133 <pandrio133@gmail.com>
2025-03-04 18:24:09 -0500

    metodo conexion se conecta con la bd de manera optimizada

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//estructura
-using System.Data.SqlClient;
+//using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
 
 //logica
@@ -7,9 +7,47 @@ namespace pruebaConexion
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-                
+            Conectar c = new Conectar();
+            
         }
+
+    }
+
+    class Conectar
+    {
+        public Conectar(){}
+
+        String database { set;get; } 
+        String user { set; get; } 
+        String password{set; get; }
+                
+        public SqlConnection Conexion()
+        {
+            database = "PruebaConexion";
+            user = "sa";
+            password = "159753";
+            String LineaConexion = "Server=localhost;Database="+database+";User Id="+user+";Password="+password;
+
+            using (SqlConnection conexion = new SqlConnection(LineaConexion))
+            {
+                conexion.Open();
+                return conexion;
+            }
+        }
+
+        public void MostrarNombres()
+        {
+            using (SqlConnection connection = Conexion())
+            {
+                String query = "";
+            };
+        }
+
+        
+
+
     }
 }
